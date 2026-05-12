9499a475fa76f116560ab8a073a4c0a220cbb07d
joseph <jvpretell27@outlook.com>
2026-05-11 19:26:32 -0500

    probando la mrd

---
+++ nodeapp/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) =>{
+    res.send("Service está levanrado y corriendo");
+
+});
+
+app.listen(8080, () => {
+    console.log("Server está levantado");
+
+});

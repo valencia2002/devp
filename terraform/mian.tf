f17b93189c9ce5e6b082a4380d35843464da96e1
joseph <jvpretell27@outlook.com>
2026-05-11 21:35:04 -0500

    el rol es: EC2-ECR-AUTH, line: 35

--- terraform/mian.tf
+++ terraform/mian.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "servernode" {
 }
 resource "aws_iam_instance_profile" "ec2-profile" {
   name = "ec2-profile"
-  role = "ECR-LOGIN-AUTO"
+  role = "EC2-ECR-AUTH"
 }
 resource "aws_security_group" "maingroup" {
   egress = [
